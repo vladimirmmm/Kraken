b951abbd81fb070f1ebdfc37f96827b072394a59
vladimirmmm <vladi@live.com>
2016-10-19 17:12:02 +0300

    Attempt 7.1

---
+++ Engine/TaxonomyEngine.cs
@@ -0,0 +1,136 @@
+﻿using LogicalModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace Engine
+{
+    public class TaxonomyEventArgs : EventArgs 
+    {
+        public string FilePath = "";
+        public TaxonomyEventArgs() 
+        {
+
+        }
+        public TaxonomyEventArgs(string filepath)
+        {
+            this.FilePath = filepath;
+        }
+    }
+    public abstract class TaxonomyEngine:AppEngine
+    {
+        public Instance CurrentInstance = null;
+        public Taxonomy CurrentTaxonomy = null;
+        public static TaxonomyEngine CurrentEngine = null;
+
+        public delegate void TaxonomyEventHandler(object sender, TaxonomyEventArgs e);
+        public event TaxonomyEventHandler TaxonomyLoad;
+        public event TaxonomyEventHandler TaxonomyLoadFailed;
+        public event TaxonomyEventHandler TaxonomyLoaded;
+        public event TaxonomyEventHandler InstanceLoad;
+        public event TaxonomyEventHandler InstanceLoadFailed;
+        public event TaxonomyEventHandler InstanceLoaded;
+
+        protected Boolean IsInstanceLoading = false;
+
+        public string HtmlPath = AppDomain.CurrentDomain.BaseDirectory + "UI.html";
+        
+        public static string PreparedFolder = "Taxonomies";
+        private static string _LocalFolder = @"";
+        public static string LocalFolder
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_LocalFolder))
+                {
+                    var appdatafolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    var entryassembly = Assembly.GetEntryAssembly();
+                    var name = "X-TreeMe";
+                    if (entryassembly != null) { name = Assembly.GetEntryAssembly().GetName().Name; }
+                    var folder = String.Format(@"{0}\{1}\", appdatafolder, name);
+                    //Utilities.FS.DirectoryCopy(PreparedFolder, folder, true, false);
+                    var taxfolder = folder + "Taxonomies\\";
+                    Utilities.FS.EnsurePath(taxfolder);
+                    _LocalFolder = taxfolder;
+                }
+                return _LocalFolder;
+            }
+            set
+            {
+                _LocalFolder = value;
+            }
+        }
+
+        public TaxonomyEngine() 
+        {
+            if (CurrentEngine == null)
+            {
+                CurrentEngine = this;
+            }
+            Logger.WriteLine(LocalFolder);
+        }
+        public virtual bool LoadInstance(string filepath) 
+        {
+            return true;
+        }
+        public virtual bool LoadTaxonomy(string filepath)
+        {
+            return true;
+
+        }
+
+        public virtual bool Trigger_TaxonomyLoad(string filepath)
+        {
+            if (TaxonomyLoad != null)
+            {
+                TaxonomyLoad(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+        public virtual bool Trigger_TaxonomyLoadFailed(string filepath)
+        {
+            if (TaxonomyLoadFailed != null)
+            {
+                TaxonomyLoadFailed(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+        public virtual bool Trigger_TaxonomyLoaded(string filepath)
+        {           
+            if (TaxonomyLoaded != null)
+            {
+                TaxonomyLoaded(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+
+        public virtual bool Trigger_InstanceLoad(string filepath)
+        {
+            if (InstanceLoad != null)
+            {
+                InstanceLoad(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+        public virtual bool Trigger_InstanceLoadFailed(string filepath)
+        {
+            if (InstanceLoadFailed != null)
+            {
+                InstanceLoadFailed(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+        public virtual bool Trigger_InstanceLoaded(string filepath)
+        {
+            if (InstanceLoaded != null)
+            {
+                InstanceLoaded(this, new TaxonomyEventArgs(filepath));
+            }
+            return true;
+        }
+    }
+}
---
+++ Model/Models/LayoutLink.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalModel.Models
+{
+    public class LayoutLink
+    {
+        public LayoutItem From = null;
+        public LayoutItem To = null;
+        public int Order = 0;
+        public string Role = "";
+        public void test() 
+        {
+            var s = new List<string>() { "[eba_dim:MCY]eba_MC:x415", "[eba_dim:MCS]eba_MC:x990" };
+            var s2 = new List<string>() { "y", "s" };
+            var z = s.OrderBy(i => i).ToList();
+            var z2 = s2.OrderBy(i => i).ToList();
+            s = s.OrderBy(i => i).ToList();
+            var x1 = s.OrderBy(i => i, StringComparer.InvariantCulture).ToList();
+            var x2 = s.OrderBy(i => i, StringComparer.CurrentCulture).ToList();
+            var x3 = s.OrderBy(i => i, StringComparer.Ordinal).ToList();
+            var x4 = s.OrderBy(i => i, StringComparer.OrdinalIgnoreCase).ToList();
+
+        }
+    }
+}
